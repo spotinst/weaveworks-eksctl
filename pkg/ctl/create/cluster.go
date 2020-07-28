7e6a347691e2bd74d184a1110f7590d40d385011
liranp <liran.polak@gmail.com>
2020-07-28 11:18:13 +0300

    feat(nodegroup): new integration: spot ocean

--- pkg/ctl/create/cluster.go
+++ pkg/ctl/create/cluster.go
@@ -8,21 +8,20 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"k8s.io/client-go/tools/clientcmd"
-
-	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils/filter"
-
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/authconfigmap"
 	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils"
+	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils/filter"
 	"github.com/weaveworks/eksctl/pkg/eks"
 	"github.com/weaveworks/eksctl/pkg/gitops"
 	"github.com/weaveworks/eksctl/pkg/kops"
 	"github.com/weaveworks/eksctl/pkg/printers"
+	"github.com/weaveworks/eksctl/pkg/spot"
 	"github.com/weaveworks/eksctl/pkg/utils/kubeconfig"
 	"github.com/weaveworks/eksctl/pkg/utils/kubectl"
 	"github.com/weaveworks/eksctl/pkg/utils/names"
 	"github.com/weaveworks/eksctl/pkg/vpc"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func createClusterCmd(cmd *cmdutils.Cmd) {
@@ -80,6 +79,11 @@ func createClusterCmdWithRunFunc(cmd *cmdutils.Cmd, runFunc func(cmd *cmdutils.C
 		}
 		fs.StringVar(&params.KopsClusterNameForVPC, "vpc-from-kops-cluster", "", "re-use VPC from a given kops cluster")
 		fs.StringVar(cfg.VPC.NAT.Gateway, "vpc-nat-mode", api.ClusterSingleNAT, "VPC NAT mode, valid options: HighlyAvailable, Single, Disable")
+	})
+
+	cmd.FlagSetGroup.InFlagSet("Spot", func(fs *pflag.FlagSet) {
+		cmdutils.AddSpotOceanCommonFlags(fs, &params.SpotProfile)
+		cmdutils.AddSpotOceanCreateNodeGroupFlags(fs, &params.SpotOcean)
 	})
 
 	cmdutils.AddCommonFlagsForAWS(cmd.FlagSetGroup, &cmd.ProviderConfig, true)
@@ -307,7 +311,10 @@ func doCreateCluster(cmd *cmdutils.Cmd, ng *api.NodeGroup, params *cmdutils.Crea
 			return err
 		}
 		postClusterCreationTasks := ctl.CreateExtraClusterConfigTasks(cfg, params.InstallWindowsVPCController)
-		tasks := stackManager.NewTasksToCreateClusterWithNodeGroups(cfg.NodeGroups, cfg.ManagedNodeGroups, supportsManagedNodes, postClusterCreationTasks)
+		tasks, err := stackManager.NewTasksToCreateClusterWithNodeGroups(cfg.NodeGroups, cfg.ManagedNodeGroups, supportsManagedNodes, postClusterCreationTasks)
+		if err != nil {
+			return fmt.Errorf("failed to create cluster tasks: %v", err)
+		}
 
 		logger.Info(tasks.Describe())
 		if errs := tasks.DoAllSync(); len(errs) > 0 {
@@ -369,7 +376,26 @@ func doCreateCluster(cmd *cmdutils.Cmd, ng *api.NodeGroup, params *cmdutils.Crea
 		}
 		logger.Success("all EKS cluster resources for %q have been created", meta.Name)
 
+		// Spot Ocean.
+		{
+			// Initialize a new raw REST client.
+			rawClient, err := ctl.NewRawClient(cfg)
+			if err != nil {
+				return err
+			}
+
+			// Execute post-creation actions.
+			if err := spot.RunPostCreation(cfg, clientSet, rawClient, true); err != nil {
+				return err
+			}
+		}
+
 		for _, ng := range cfg.NodeGroups {
+			// skip Spot Ocean nodegroups
+			if ng.SpotOcean != nil {
+				continue
+			}
+
 			// authorise nodes to join
 			if err = authconfigmap.AddNodeGroup(clientSet, ng); err != nil {
 				return err
