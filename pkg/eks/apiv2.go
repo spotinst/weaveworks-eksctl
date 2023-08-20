86fb503c76b051c61a3cf9c47d4925a8364ddf31
idanshoham <102898343+IdanShohamNetApp@users.noreply.github.com>
2023-08-20 10:06:00 +0300

    Release 0.148 (#501)

--- pkg/eks/apiv2.go
+++ pkg/eks/apiv2.go
@@ -48,6 +48,7 @@ func newV2Config(pc *api.ProviderConfig, credentialsCacheFilePath string, config
 	if pc.Region != "" {
 		options = append(options, config.WithRegion(pc.Region))
 	}
+	options = append(options, config.WithRegion(pc.Region))
 	clientLogMode := aws.ClientLogMode(1)
 
 	if logger.Level >= api.AWSDebugLevel {
