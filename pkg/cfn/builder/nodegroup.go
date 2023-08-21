d969e225d767a9aa7e26701d9c6f24a21f74eaa3
idanshoham <102898343+IdanShohamNetApp@users.noreply.github.com>
2023-08-21 09:48:30 +0300

    Minor fixes (#543)

--- pkg/cfn/builder/nodegroup.go
+++ pkg/cfn/builder/nodegroup.go
@@ -1198,6 +1198,16 @@ func (n *NodeGroupResourceSet) newNodeGroupSpotOceanVirtualNodeGroupResource(lau
 		spec.RestrictScaleDown = restrictScaleDown
 	}
 
+	// Restrict Scale Down.
+	if restrictScaleDown := n.spec.SpotOcean.RestrictScaleDown; restrictScaleDown != nil {
+		spec.RestrictScaleDown = restrictScaleDown
+	}
+
+	// Restrict Scale Down.
+	if restrictScaleDown := n.spec.SpotOcean.RestrictScaleDown; restrictScaleDown != nil {
+		spec.RestrictScaleDown = restrictScaleDown
+	}
+
 	return &spot.ResourceNodeGroup{
 		VirtualNodeGroup: spec,
 		Resource: spot.Resource{
