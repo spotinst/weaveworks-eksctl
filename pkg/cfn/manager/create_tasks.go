b8eea7bc1975d595983dcf5aa917881c7df8acf0
idanshoham <idan.shoham@netapp.com>
2023-10-19 12:14:03 +0300

    sync spot-ocean changes with the new version

--- pkg/cfn/manager/create_tasks.go
+++ pkg/cfn/manager/create_tasks.go
@@ -41,70 +41,38 @@ func (c *StackCollection) NewTasksToCreateClusterWithNodeGroups(ctx context.Cont
 		)
 	}
 
-/*	// Nodegroups. old version TODO idan - check changes
+	// Nodegroups.
 	{
 		vpcImporter := vpc.NewStackConfigImporter(c.MakeClusterStackName())
-		nodeGroupTaskTree, err := c.NewNodeGroupTask(ctx, nodeGroups, managedNodeGroups, false, vpcImporter)
+		nodeGroupTasks, err := c.NewNodeGroupTask(ctx, nodeGroups, managedNodeGroups, false, false, vpcImporter)
 		if err != nil {
 			return nil, err
 		}
 
-		if nodeGroupTaskTree.Len() > 0 {
-			nodeGroupTaskTree.IsSubTask = true
-			taskTree.Append(nodeGroupTaskTree)
+		if nodeGroupTasks.Len() > 0 {
+			nodeGroupTasks.IsSubTask = true
+			taskTree.Append(nodeGroupTasks)
 		}
 	}
 
 	// Post creation tasks.
 	{
 		if len(postClusterCreationTasks) > 0 {
-			postTaskTree := &tasks.TaskTree{
+			postClusterCreationTaskTree := &tasks.TaskTree{
 				Parallel:  false,
 				IsSubTask: true,
 			}
-			postTaskTree.Append(postClusterCreationTasks...)
-			taskTree.Append(postTaskTree)
+			postClusterCreationTaskTree.Append(postClusterCreationTasks...)
+			taskTree.Append(postClusterCreationTaskTree)
 		}
-	}*/
-
-	appendNodeGroupTasksTo := func(taskTree *tasks.TaskTree) {
-		vpcImporter := vpc.NewStackConfigImporter(c.MakeClusterStackName())
-		nodeGroupTasks := &tasks.TaskTree{
-			Parallel:  true,
-			IsSubTask: true,
-		}
-		if unmanagedNodeGroupTasks := c.NewUnmanagedNodeGroupTask(ctx, nodeGroups, false, false, vpcImporter); unmanagedNodeGroupTasks.Len() > 0 {
-			unmanagedNodeGroupTasks.IsSubTask = true
-			nodeGroupTasks.Append(unmanagedNodeGroupTasks)
-		}
-		if managedNodeGroupTasks := c.NewManagedNodeGroupTask(ctx, managedNodeGroups, false, vpcImporter); managedNodeGroupTasks.Len() > 0 {
-			managedNodeGroupTasks.IsSubTask = true
-			nodeGroupTasks.Append(managedNodeGroupTasks)
-		}
-
-		if nodeGroupTasks.Len() > 0 {
-			taskTree.Append(nodeGroupTasks)
-		}
-	}
-
-	if len(postClusterCreationTasks) > 0 {
-		postClusterCreationTaskTree := &tasks.TaskTree{
-			Parallel:  false,
-			IsSubTask: true,
-		}
-		postClusterCreationTaskTree.Append(postClusterCreationTasks...)
-		appendNodeGroupTasksTo(postClusterCreationTaskTree)
-		taskTree.Append(postClusterCreationTaskTree)
-	} else {
-		appendNodeGroupTasksTo(&taskTree)
 	}
 
 	return &taskTree, nil
 }
 
-// NewNodeGroupTask defines tasks required to create all of the nodegroups
+// NewNodeGroupTask defines tasks required to create all the nodegroups
 func (c *StackCollection) NewNodeGroupTask(ctx context.Context, nodeGroups []*api.NodeGroup, managedNodeGroups []*api.ManagedNodeGroup,
-	forceAddCNIPolicy bool, vpcImporter vpc.Importer) (*tasks.TaskTree, error) {
+	forceAddCNIPolicy bool, skipEgressRules bool, vpcImporter vpc.Importer) (*tasks.TaskTree, error) {
 	taskTree := &tasks.TaskTree{Parallel: true}
 
 	// Spot Ocean.
@@ -131,7 +99,7 @@ func (c *StackCollection) NewNodeGroupTask(ctx context.Context, nodeGroups []*ap
 
 	// Unmanaged.
 	{
-		nodeGroupTaskTree := c.NewUnmanagedNodeGroupTask(ctx, nodeGroups, forceAddCNIPolicy, vpcImporter)
+		nodeGroupTaskTree := c.NewUnmanagedNodeGroupTask(ctx, nodeGroups, forceAddCNIPolicy, skipEgressRules, vpcImporter)
 		if nodeGroupTaskTree.Len() > 0 {
 			nodeGroupTaskTree.IsSubTask = true
 			taskTree.Append(nodeGroupTaskTree)
